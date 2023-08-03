5709d8d1bafa019ae39fa83458ae362e9231c622
Asteises <p.nikitaev@gmail.com>
2023-08-03 12:31:34 +0300

    init commit

---
+++ src/main/java/ru/asteises/nvatest/NvaTestApplication.java
@@ -0,0 +1,13 @@
+package ru.asteises.nvatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NvaTestApplication.class, args);
+    }
+
+}
